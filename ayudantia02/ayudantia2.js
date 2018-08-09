13d9656aa68c573a05c1d1c231a5e515ec8be03e
crabarca <caabarca1@uc.cl>
2018-08-08 20:32:24 -0400

    Ayudantia2 files

---
+++ ayudantia02/ayudantia2.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function(){ 
+    const boton1 = document.getElementById("boton1");
+    boton1.onclick = function(d) {
+        console.log("Hice click en " + boton1.value)
+    }
+
+    const boton2 = document.getElementById("boton2");
+    boton2.onclick = function(d) {
+        console.log("Hice click en " + boton2.value)
+    }
+
+    const boton3 = document.getElementById("boton3");
+    boton3.onclick = function(d) {
+        console.log("Hice click en " + boton3.value)
+    }
+
+    const botonHide = document.getElementById("botonHide");
+    const texto = document.getElementById("ejemplo2");
+    botonHide.onclick = function(d) {
+        texto.style.display = "none"
+    }
+
+    const botonShow = document.getElementById("botonShow");
+    botonShow.onclick = function(d) {
+        texto.style.display = "block"
+    }
+
+    const botonEditar = document.getElementById("botonEditar");
+    botonEditar.onclick = function(d) {
+        document.getElementById("ejemplo3").innerText = document.getElementById("textoEjemplo3").value
+    }
+    
+}, false);

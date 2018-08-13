d405eedbf40641e611237edca85a2c67e8da2d1e
hernan4444 <hfvaldivieso@uc.cl>
2018-08-13 08:57:07 -0300

    Upload ayudantia02 :star:

--- ayudantia02/ayudantia2.js
+++ ayudantia02/ayudantia2.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     // Ejemplo 1
     const boton1 = document.getElementById("boton1");
     boton1.onclick = function(d) {
+        console.log(d);
         console.log("Hice click en " + boton1.value)
     }
 
@@ -32,6 +33,18 @@ document.addEventListener('DOMContentLoaded', function(){
     const botonEditar = document.getElementById("botonEditar");
     botonEditar.onclick = function(d) {
         document.getElementById("ejemplo3").innerText = document.getElementById("textoEjemplo3").value
+    }
+
+    // Ejemplo 4
+    const color1 = document.getElementById("color_1");
+    const color2 = document.getElementById("color_2");
+    color1.onclick = function(d) {
+        const body = document.getElementsByTagName("body")[0];
+        body.style.backgroundColor = "Magenta";
+    }
+    color2.onclick = function(d) {
+        const body = document.getElementsByTagName("body")[0];
+        body.style.backgroundColor = "White";
     }
     
 }, false);

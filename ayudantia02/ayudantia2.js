dba5f623814215c9bea70530f40a88714c4947f6
crabarca <caabarca1@uc.cl>
2018-08-22 17:36:46 -0300

    :bug: Arrow functions added

--- ayudantia02/ayudantia2.js
+++ ayudantia02/ayudantia2.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){ 
+document.addEventListener('DOMContentLoaded', () => { 
     // Ejemplo 1
     const boton1 = document.getElementById("boton1");
-    boton1.onclick = function(d) {
+    boton1.onclick = (d) => {
         console.log(d);
         console.log("Hice click en " + boton1.value)
     }
 
     const boton2 = document.getElementById("boton2");
-    boton2.onclick = function(d) {
+    boton2.onclick = (d) => {
         console.log("Hice click en " + boton2.value)
     }
 
     const boton3 = document.getElementById("boton3");
-    boton3.onclick = function(d) {
+    boton3.onclick = (d) => {
         console.log("Hice click en " + boton3.value)
     }
 
@@ -20,29 +20,29 @@ document.addEventListener('DOMContentLoaded', function(){
     // Ejemplo 2
     const botonHide = document.getElementById("botonHide");
     const texto = document.getElementById("ejemplo2");
-    botonHide.onclick = function(d) {
+    botonHide.onclick = (d) => {
         texto.style.display = "none"
     }
 
     const botonShow = document.getElementById("botonShow");
-    botonShow.onclick = function(d) {
+    botonShow.onclick = (d) => {
         texto.style.display = "block"
     }
 
     // Ejemplo 3
     const botonEditar = document.getElementById("botonEditar");
-    botonEditar.onclick = function(d) {
+    botonEditar.onclick = (d) => {
         document.getElementById("ejemplo3").innerText = document.getElementById("textoEjemplo3").value
     }
 
     // Ejemplo 4
     const color1 = document.getElementById("color_1");
     const color2 = document.getElementById("color_2");
-    color1.onclick = function(d) {
+    color1.onclick = (d) => {
         const body = document.getElementsByTagName("body")[0];
         body.style.backgroundColor = "Magenta";
     }
-    color2.onclick = function(d) {
+    color2.onclick = (d) => {
         const body = document.getElementsByTagName("body")[0];
         body.style.backgroundColor = "White";
     }
